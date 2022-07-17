83c12e6568b07fc6ce3c9398cd35204b00dd075e
Pasero Christian <c.pasero@student.tugraz.at>
2022-07-18 01:13:30 +0200

    fucking handy other then chromium

--- code.js
+++ code.js
@@ -5,12 +5,7 @@ gsap.registerPlugin(MotionPathPlugin);
 
 gsap.defaults({ease: "none"});
 
-let pulses = {};
-let duration_ = 1;
-if (window.innerWidth > 1000)
-{
-  console.log(window.innerWidth);
-pulses = gsap.timeline({
+const pulses = gsap.timeline({
   defaults: {
     scale: 2,
     autoAlpha:1,
@@ -25,26 +20,6 @@ pulses = gsap.timeline({
 .to("#ball2010, .T2010", {}, 0.8)
 .to("#ball1999, .T1999", {}, 1)
 .to("#ende", {}, 2)
-}
-else {
-  console.log("helo");
-  pulses = gsap.timeline({
-    defaults: {
-      scale: 2,
-      autoAlpha:1,
-      transformOrigin: 'center', 
-      ease: "elastic(2.5, 1.5)"
-    }})
-  .to("#ballNow,  .Now", {}, 0.5) 
-  .to("#ball2022, .T2022", {}, 0.55)
-  .to("#ball2018, .T2018", {}, 0.75)
-  .to("#ball2017, .T2017", {}, 0.8)
-  .to("#ball2013, .T2013", {}, 1.02)
-  .to("#ball2010, .T2010", {}, 1.3)
-  .to("#ball1999, .T1999", {}, 1.5)
-  .to("#ende", {}, 2)
-  duration_ = 3;
-}
 
 
 const main = gsap.timeline({
@@ -55,5 +30,5 @@ const main = gsap.timeline({
     end: "bottom center"
   }
 })
-.from(".theLine", {drawSVG:0, duration:duration_}, 0)
+.from(".theLine", {drawSVG:-10, duration:1}, 0)
 .add(pulses, 0)
